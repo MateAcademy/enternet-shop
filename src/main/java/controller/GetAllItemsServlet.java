74b76133f3cf58810796414280504751e97f0623
Sergey Klunniy <s.klunniy@gmail.com>
2022-09-14 17:06:01 +0300

    commit

--- src/main/java/controller/GetAllItemsServlet.java
+++ src/main/java/controller/GetAllItemsServlet.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 public class GetAllItemsServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
         super.doGet(req, resp);
     }
 
